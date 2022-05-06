bf86583e35752ad260781054941b5478967752ad
mdsco2 <marcosdsco2@gmail.com>
2022-05-05 21:07:27 -0400

    New endpoints - \hora - \soma

--- app/views.py
+++ app/views.py
@@ -8,3 +8,12 @@ def home():
 @app.route('/template')
 def template():
     return render_template('home.html')
+
+@app.route('/hora')
+def hora():
+    from datetime import datetime
+    return f'O horário atual do servidor: {datetime.now()}'
+
+@app.route('/soma/<int:n1>/<int:n2>' )
+def soma(n1=5, n2=5):
+    return f'A soma entre {n1} e {n2} é igual a {n1 + n2}'
